9d862dfe64f105d187174052b19570a2280e2c36
Kevin Griego <Kevin.Greigo@aexp.com>
2019-08-03 01:27:51 -0700

    some weird updates

--- src/Components/PixiContainer.jsx
+++ src/Components/PixiContainer.jsx
@@ -1,5 +1,6 @@
 /* Library Imports */
 import React from "react";
+import { TweenMax, Sine } from "gsap";
 import { Stage, Container } from "react-pixi-fiber";
 import * as PIXI from "pixi.js";
 
@@ -20,14 +21,20 @@ class PixiContainer extends React.Component {
     this.getValues();
   }
 
+  getValues = () => {
+    const height = window.innerHeight * 2;
+    const width = window.innerWidth * 2;
+    this.setState({ height, width });
+  };
+
   makeImages = () => {
     this.container.buttonMode = true;
     tileData.map((image, i) => {
       const imageToRender = new PIXI.Sprite.from(image.src.src);
-      if (i === 0) {
+      if (i === 9) {
         imageToRender.x = 0;
         imageToRender.y = 0;
-      } else if (i === 1) {
+      } else if (i === 13) {
         imageToRender.x = tileData[i - 1].src.width + 10;
         imageToRender.y = 0;
       } else {
@@ -37,20 +44,22 @@ class PixiContainer extends React.Component {
     });
   };
 
-  getValues = () => {
-    const height = window.innerHeight;
-    const width = window.innerWidth;
-    this.setState({ height, width });
-  };
+  scaleNormal = () => TweenMax.to(this.stage, 1, { scale: 1, ease: Sine.ease });
+  scaleDown = () => TweenMax.to(this.stage, 1, { scale: 0.9 });
+
   render() {
     const { height, width } = this.state;
     return (
       <Stage
         height={height}
         interactive
+        mousedown={this.scaleDown}
         mousemove={onMouseMove}
+        mouseout={this.scaleNormal}
+        mouseup={this.scaleNormal}
         options={{ backgroundColor: 0x10bb99 }}
         ref={ref => (this.stage = ref)}
+        style={{ height: window.innerHeight, width: window.innerWidth }}
         width={width}
       >
         <Container
@@ -96,15 +105,11 @@ function onMouseMove(e) {
 
 function onDragMove(e) {
   if (this.dragging) {
-    const newPosition = this.data.getLocalPosition(this.parent);
-    const x = newPosition.x - this._xLast;
-    const y = newPosition.y - this._yLast;
-    const newX = x * 0.5;
-    const newY = y * 0.5;
+    const x = this.data.originalEvent.pageX - this.lastX;
+    const y = this.data.originalEvent.pageY - this.lastY;
 
-    this.position.x = newX;
-    this.position.y = newY;
-    // debugger;
+    this.position.x = x;
+    this.position.y = y;
   }
 }
 function onDragStart(e) {
@@ -113,8 +118,13 @@ function onDragStart(e) {
   // we want to track the movement of this particular touch
   this.data = e.data;
   this.dragging = true;
-  this._xLast = this.position.x;
-  this._yLast = this.position.y;
+  this.lastX =
+    this.data.originalEvent.pageX -
+    this.data.originalEvent.currentTarget.offsetLeft;
+
+  this.lastY =
+    this.data.originalEvent.pageY -
+    this.data.originalEvent.currentTarget.offsetTop;
 }
 
 function onDragEnd() {
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,5 @@
 /* Library Imports */
 import React from "react";
-import { TweenMax, Sine } from "gsap";
 
 /* Redux Imports */
 
@@ -11,29 +10,12 @@ class About extends React.Component {
   constructor(props) {
     super(props);
     this.logoElement = React.createRef();
-    this._xLast = 0;
-    this._yLast = 0;
-    this._xTo = 0;
-    this._yTo = 0;
     this.state = {};
   }
-
-  scaleNormal = () =>
-    TweenMax.to(this.logoElement, 1, { scale: 1, ease: Sine.ease });
-  scaleDown = e => {
-    this._xLast = e.clientX;
-    this._yLast = e.clientY;
-    TweenMax.to(this.logoElement, 1, { scale: 0.9 });
-  };
-
   render() {
     return (
       <div className={"App"}>
         <div
-          onMouseDown={this.scaleDown}
-          onMouseLeave={this.scaleNormal}
-          onMouseUp={this.scaleNormal}
-          ref={element => (this.logoElement = element)}
           style={{
             border: "2px black solid",
             height: "100vh",
